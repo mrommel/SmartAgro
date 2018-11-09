5756f073a33b1b67b1b012d17b6a98b0192a7099
Michael Rommel <rommel@365farmnet.com>
2018-11-09 09:24:37 +0100

    fix static files

--- smartagro/smartagro/urls.py
+++ smartagro/smartagro/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^core/', include('core.urls')),
     url(r'^accounts/', include('accounts.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
